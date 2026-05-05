21555fe376845508826a645f832d098be690cf83
camilarevth <camilafcostacurta@gmail.com>
2026-05-05 13:14:59 -0300

    Create main.dart

---
+++ mobile/lib/features/wallet/presentation/main.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mobile/core/routes/app_routes.dart';
+import 'package:mobile/features/dashboard/main_wrapper_screen.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MesclaInvest',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF512DA8)),
+      ),
+      // Ponto de entrada ajustado para o Balcão via MainWrapper
+      home: const MainWrapperScreen(),
+      // Todas as rotas (incluindo as novas)
+      routes: AppRoutes.routes,
+    );
+  }
+}
